adefe51ae1dbc68112490a59a677714c7d692278
Michael <mpgg2000@gmail.com>
2023-01-17 12:07:36 +0300

    debug webhooks

--- lead_orchestrator/webhook_routes.py
+++ lead_orchestrator/webhook_routes.py
@@ -1,15 +1,15 @@
 from .route_classes import QSEncodedRoute
 from fastapi import Request, Response, APIRouter
-
+from .amo.models import WebHook
 
 amo_router = APIRouter(route_class=QSEncodedRoute)
 
 
 @amo_router.post("/tasks/webhook")
-async def task_webhook(request: Request):
+async def task_webhook(hook: WebHook, request: Request):
     print("Hook recieved")
-    print(await request.body())
-    print(request.client)
     print(await request.json())
+    print(request.headers)
+    print(hook)
 
     return Response(status_code=200)
