a81656574a80b04884c441dce1a04b0f793157db
Michael <mpgg2000@gmail.com>
2023-01-17 12:39:53 +0300

    debug webhooks

--- lead_orchestrator/route_classes.py
+++ lead_orchestrator/route_classes.py
@@ -6,14 +6,27 @@ import ujson
 from fastapi import Request, Response
 from fastapi.routing import APIRoute
 
+from starlette.datastructures import Headers
+
 
 class QSRequest(Request):
     async def body(self) -> bytes:
         if not hasattr(self, "_body"):
             body = await super().body()
             body = qsparser.parse(unquote(body.decode("utf-8")))
-            self._body = ujson.dumps(body, encode_html_chars=False, ensure_ascii=False).encode()
+            self._body = ujson.dumps(
+                body, encode_html_chars=False, ensure_ascii=False).encode()
         return self._body
+
+    @property
+    def headers(self) -> Headers:
+        if not hasattr(self, "_headers"):
+            self._headers = Headers(
+                headers={
+                    "content-type": "application/json"
+                },
+                scope=self.scope)
+        return self._headers
 
 
 class QSEncodedRoute(APIRoute):
